1f8428f5637edd632260c31ab30cdfc87f818807
dombroks <tinoutech@gmail.com>
2021-03-13 13:32:20 +0100

    Implementing get started screnn

--- lib/view/getStarted.dart
+++ lib/view/getStarted.dart
@@ -1,18 +1,57 @@
+import 'package:delivery/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class GetStartedScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          SvgPicture.asset("assets/images/bike.svg"),
-          Text("Express Delivery"),
-          Text("Join our delivery team")
-        ],
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SvgPicture.asset("assets/images/bike.svg"),
+            Column(
+              children: [
+                Text("Express Delivery",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 26,
+                      fontWeight: FontWeight.bold,
+                    )),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Join our delivery team",
+                  style: TextStyle(color: Colors.grey),
+                )
+              ],
+            ),
+            SizedBox(
+              height: size.height * 0.3,
+            ),
+            SizedBox(
+              width: 300,
+              height: 45,
+              child: RaisedButton(
+                onPressed: () {},
+                color: kPrimaryColor,
+                child: Text(
+                  "Get Started",
+                  style: TextStyle(color: Colors.white, fontSize: 18),
+                ),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(2)),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            )
+          ],
+        ),
       ),
     );
   }
