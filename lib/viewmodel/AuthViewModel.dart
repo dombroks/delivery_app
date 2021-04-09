953d38b35ca72253f29a2974b29631d53fe16285
dombroks <tinoutech@gmail.com>
2021-04-09 22:41:59 +0100

    some refactoring

--- lib/viewmodel/AuthViewModel.dart
+++ lib/viewmodel/AuthViewModel.dart
@@ -1,4 +1,4 @@
-import 'package:delivery/utils/Repository.dart';
+import 'package:delivery/data/Repository.dart';
 import 'package:flutter/material.dart';
 
 class AuthViewModel extends ChangeNotifier {
