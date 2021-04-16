474b6e4cdbb09e7b1ddf7b92ca72757536c9f950
dombroks <tinoutech@gmail.com>
2021-04-16 01:41:56 +0100

    working on commands screen

---
+++ lib/view/component/CommandWidget.dart
@@ -0,0 +1,160 @@
+import 'package:delivery/utils/constants.dart';
+import 'package:expandable/expandable.dart';
+import 'package:flutter/material.dart';
+
+import 'CustomButton.dart';
+
+class CommandWidget extends StatelessWidget {
+  const CommandWidget({
+    Key key,
+    this.imageUrl,
+    this.clientName,
+    this.price,
+    this.distance,
+    this.pickUpAddress,
+    this.dropOffAddress,
+  }) : super(key: key);
+  final String imageUrl;
+  final String clientName;
+  final String price;
+  final String distance;
+  final String pickUpAddress;
+  final String dropOffAddress;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(
+          color: kPrimaryColor,
+        ),
+      ),
+      child: ExpandablePanel(
+        hasIcon: false,
+        header: Container(
+          decoration: BoxDecoration(
+            color: Colors.grey[300],
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(9.0),
+            child: Row(
+              children: [
+                Container(
+                  width: 50,
+                  height: 50,
+                  child: Image.asset(
+                    "assets/images/someone.png",
+                    fit: BoxFit.fill,
+                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            clientName,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          )
+                        ],
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text(
+                            "\$$price",
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 18),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            "$distance km",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        collapsed: Container(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "PICK UP",
+                  style: TextStyle(
+                      color: Colors.grey, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(pickUpAddress,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17)),
+                SizedBox(
+                  height: 6,
+                ),
+                Container(
+                  height: 1,
+                  color: Colors.grey,
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  "DROP OFF",
+                  style: TextStyle(
+                      color: Colors.grey, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(dropOffAddress,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17)),
+                SizedBox(
+                  height: 15,
+                ),
+                CustomButton(
+                  text: "Accept",
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/view/screen/homeScreen.dart
+++ lib/view/screen/homeScreen.dart
@@ -1,7 +1,5 @@
-import 'package:delivery/utils/constants.dart';
-import 'package:delivery/view/component/CustomButton.dart';
+import 'package:delivery/view/component/CommandWidget.dart';
 import 'package:delivery/view/screen/sideDrawer.dart';
-import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -49,156 +47,46 @@ class HomeScreen extends StatelessWidget {
                 SizedBox(
                   height: 10,
                 ),
-                CommandWidget(),
-                CommandWidget(),
-                CommandWidget(),
-                CommandWidget(),
-                CommandWidget(),
-                CommandWidget(),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class CommandWidget extends StatelessWidget {
-  const CommandWidget({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 8),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(10),
-        border: Border.all(
-          color: kPrimaryColor,
-        ),
-      ),
-      child: ExpandablePanel(
-        hasIcon: false,
-        header: Container(
-          decoration: BoxDecoration(
-            color: Colors.grey[300],
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(10),
-              topRight: Radius.circular(10),
-              bottomLeft: Radius.circular(10),
-              bottomRight: Radius.circular(10),
-            ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(9.0),
-            child: Row(
-              children: [
-                Container(
-                  width: 50,
-                  height: 50,
-                  child: Image.asset(
-                    "assets/images/someone.png",
-                    fit: BoxFit.fill,
-                  ),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                  ),
+                CommandWidget(
+                  clientName: "Joe Hogg",
+                  pickUpAddress:
+                      "2972 Westheimer Rd. Santa Ana, Illinois 85486 ",
+                  dropOffAddress: "6391 Elgin St. Celina, Delaware 10299",
+                  distance: "3.17",
+                  price: "57.00",
                 ),
-                SizedBox(
-                  width: 10,
+                CommandWidget(
+                  clientName: "Will Smith",
+                  pickUpAddress:
+                      "2972 Westheimer Rd. Santa Ana, Illinois 85486 ",
+                  dropOffAddress: "6391 Elgin St. Celina, Delaware 10299",
+                  distance: "10.3",
+                  price: "107.23",
                 ),
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Column(
-                        children: [
-                          Text(
-                            "Joe Hogg",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          )
-                        ],
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text(
-                            "\$36.00",
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold, fontSize: 18),
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            "3.7 km",
-                            style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
+                CommandWidget(
+                  clientName: "Opera",
+                  pickUpAddress:
+                      "2972 Westheimer Rd. Santa Ana, Illinois 85486 ",
+                  dropOffAddress: "6391 Elgin St. Celina, Delaware 10299",
+                  distance: "3.17",
+                  price: "57.00",
                 ),
-              ],
-            ),
-          ),
-        ),
-        collapsed: Container(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "PICK UP",
-                  style: TextStyle(
-                      color: Colors.grey, fontWeight: FontWeight.bold),
+                CommandWidget(
+                  clientName: "Joe Hogg",
+                  pickUpAddress:
+                      "2972 Westheimer Rd. Santa Ana, Illinois 85486 ",
+                  dropOffAddress: "6391 Elgin St. Celina, Delaware 10299",
+                  distance: "3.17",
+                  price: "57.00",
                 ),
-                SizedBox(
-                  height: 5,
+                CommandWidget(
+                  clientName: "Joe Hogg",
+                  pickUpAddress:
+                      "2972 Westheimer Rd. Santa Ana, Illinois 85486 ",
+                  dropOffAddress: "6391 Elgin St. Celina, Delaware 10299",
+                  distance: "3.17",
+                  price: "57.00",
                 ),
-                Text("2972 Westheimer Rd. Santa Ana, Illinois 85486 ",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 17)),
-                SizedBox(
-                  height: 6,
-                ),
-                Container(
-                  height: 1,
-                  color: Colors.grey,
-                ),
-                SizedBox(
-                  height: 6,
-                ),
-                Text(
-                  "DROP OFF",
-                  style: TextStyle(
-                      color: Colors.grey, fontWeight: FontWeight.bold),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text("6391 Elgin St. Celina, Delaware 10299",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 17)),
-                SizedBox(
-                  height: 15,
-                ),
-                CustomButton(
-                  text: "Accept",
-                )
               ],
             ),
           ),
