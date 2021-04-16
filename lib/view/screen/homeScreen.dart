f28315052b2316bec7807e673d660cd7e4132a5e
dombroks <tinoutech@gmail.com>
2021-04-16 01:19:03 +0100

    working on commands screen

--- lib/view/screen/homeScreen.dart
+++ lib/view/screen/homeScreen.dart
@@ -1,13 +1,28 @@
-import 'package:delivery/service/multiLanguageService/app_localization.dart';
+import 'package:delivery/utils/constants.dart';
+import 'package:delivery/view/component/CustomButton.dart';
 import 'package:delivery/view/screen/sideDrawer.dart';
+import 'package:expandable/expandable.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var screenSize = MediaQuery.of(context).size;
     return Scaffold(
       drawer: SideDrawer(),
       appBar: AppBar(
+        actions: [
+          Container(
+            width: 25,
+            height: 25,
+            child:
+                CircleAvatar(child: Image.asset("assets/images/someone.png")),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+            ),
+          ),
+          SizedBox(width: 20)
+        ],
         elevation: 0,
         leading: Builder(
           builder: (context) => IconButton(
@@ -18,7 +33,162 @@ class HomeScreen extends StatelessWidget {
           ),
         ),
       ),
-      body: Container(),
+      body: Container(
+        width: screenSize.width,
+        height: screenSize.height,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "New Commands",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: kPrimaryColor,
+                  ),
+                ),
+                child: ExpandablePanel(
+                  hasIcon: false,
+                  header: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey[300],
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(10),
+                        topRight: Radius.circular(10),
+                        bottomLeft: Radius.circular(10),
+                        bottomRight: Radius.circular(10),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(9.0),
+                      child: Row(
+                        children: [
+                          Container(
+                            width: 50,
+                            height: 50,
+                            child: Image.asset(
+                              "assets/images/someone.png",
+                              fit: BoxFit.fill,
+                            ),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Column(
+                                  children: [
+                                    Text(
+                                      "Joe Hogg",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18),
+                                    ),
+                                    SizedBox(
+                                      height: 20,
+                                    )
+                                  ],
+                                ),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      "\$36.00",
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      "3.7 km",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  collapsed: Container(
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "PICK UP",
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text("2972 Westheimer Rd. Santa Ana, Illinois 85486 ",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 17)),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          Container(
+                            height: 1,
+                            color: Colors.grey,
+                          ),
+                          SizedBox(
+                            height: 6,
+                          ),
+                          Text(
+                            "DROP OFF",
+                            style: TextStyle(
+                                color: Colors.grey,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          SizedBox(
+                            height: 5,
+                          ),
+                          Text("6391 Elgin St. Celina, Delaware 10299",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 17)),
+                          SizedBox(
+                            height: 15,
+                          ),
+                          CustomButton(
+                            text: "Accept",
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
